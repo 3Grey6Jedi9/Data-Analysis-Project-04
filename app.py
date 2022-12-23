907f88b47e87ea63bc690818dd1766fa860eb723
Darth Vader <danieltarancon@gmail.com>
2022-12-23 19:43:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,45 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
+# brandcsv = with open (...file path...) as brandcsv
+
+# inventorycsv = with open (...) as inventorycsv
+
+# def add_csv(file):
+    # with open (file) as ...:
+    # .....
+
+
+def add_csv():
+    with open('/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/*DATA ANALYSIS /Unit 04/Project 04/store-inventory/brands.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            name = row[0]
+            new_brand = Brands(brand_name=name)
+            session.add(new_brand)
+        session.commit()
+
+
+
+
+
+def inventory_cleaner():
+    pass
+
+
+def app():
+    pass
+
+
+
+
+
 
 class Brands(Base):
     __tablename__ = "brands"
 
     brand_id = Column(Integer, primary_key = True)
-    brand_name = Column(String)
+    brand_name = Column('Brand Name',String)
 
     def __repr__(self):
         return f'''Brand Name: {self.brand_name}'''
@@ -27,10 +60,10 @@ class Product(Base):
     __tablename__ = "products"
 
     product_id = Column(Integer, primary_key = True)
-    product_name = Column(String)
-    product_quantity = Column(Integer)
-    product_price = Column(Integer)
-    date_updated = Column(Date)
+    product_name = Column('Name', String)
+    product_quantity = Column('Product Quantity', Integer)
+    product_price = Column('Product Price',Integer)
+    date_updated = Column('Last Updated',Date)
     brand_id = Column(Integer, ForeignKey("brands.brand_id"))
 
     def __repr__(self):
@@ -44,5 +77,10 @@ class Product(Base):
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    #app()
+    #add_csv()
+
+    for p in session.query(Brands.brand_name):
+        print(p)
 
 
